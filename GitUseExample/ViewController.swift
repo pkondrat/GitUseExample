c13b8804cd09d2e77fe24a3d4842da15978fbf33
Piotr Kondratowicz <piotr@piotrs-macbook-pro.local>
2017-04-13 15:10:35 +0200

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Piotr on 13/04/2017.
+//  Copyright © 2017 PMI Piotr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
